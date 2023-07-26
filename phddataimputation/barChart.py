d4b010416bb11fba6feb5909afc24266479cbe6d
Gohar Shoukat <25367760+goharShoukat@users.noreply.github.com>
2023-07-26 14:50:13 +0100

    feat(bar-chart): added gap quanitification for time series

--- phddataimputation/barChart.py
+++ phddataimputation/barChart.py
@@ -13,7 +13,7 @@ import numpy as np
 
 df=pd.read_csv('data/semiprocessed/wind.csv')
 cols = df.columns
-bins = [0, 1, 6, 12, 24, 48, 72, 365*24, 365*48]
+bins = [0, 1, 2, 3, 24, 48, 365*24]
 
 gaps = {}
 #extract gaps for each column and stack them in a nest list
@@ -26,7 +26,7 @@ for col in cols:
     gaps[col] = grouped
 
 
-width = 0.15
+width = 0.17
 barm2 = np.arange(len(bins) - 1)
 barm3 = [x + width for x in barm2]
 barm4 = [x + width for x in barm3]
@@ -35,14 +35,31 @@ barm6 = [x + width for x in barm5]
 
 
 fig, ax = plt.subplots(figsize=(30,30))
-ax.bar(barm2, gaps['M3'], width=width, label='M2')
-ax.bar(barm3, gaps['M2'], width=width, label='M3')
-ax.bar(barm4, gaps['M4'], width=width, label='M4')
-ax.bar(barm5, gaps['M5'], width=width, label='M5')
-ax.bar(barm6, gaps['M6'], width=width, label='M6')
+
+rects = ax.bar(barm2, gaps['M3'], width=width, label='M2')
+ax.bar_label(rects, padding=1)
+
+rects = ax.bar(barm3, gaps['M2'], width=width, label='M3')
+ax.bar_label(rects, padding=1)
+
+rects = ax.bar(barm4, gaps['M4'], width=width, label='M4')
+ax.bar_label(rects, padding=1)
+
+rects = ax.bar(barm5, gaps['M5'], width=width, label='M5')
+ax.bar_label(rects, padding=1)
+
+
+rects = ax.bar(barm6, gaps['M6'], width=width, label='M6')
+ax.bar_label(rects, padding=1)
+
 ax.set_xticks([r + 2*width for r in range(len(bins) - 1)],
-              ['1', '2 - 6', '7 - 12', '13 - 24', '25-48', '49 - 72', '73 - 1Y', '> 1Y'] )
-ax.set_xlabel('Gaps (in Hour duration)')
+              ['1H', '2H', '3H', 
+               r'$4H \,\less \,time \,\leq\, 1D$',
+               r'$1D \,\less \,time \,\leq\, 2D$', 
+               r'$2D \,\less\, time \,\leq\, 1Y$',] )
+
+ax.set_xlabel('Gaps')
 ax.set_ylabel('Occurance')
 ax.set_title('Wind Speed')
 plt.legend()
+plt.savefig('output/gap-bars.png', dpi=200)
