ccdb5754fc02e5d6b37bdb97e88515b9e3c4faa7
Gohar Shoukat <25367760+goharShoukat@users.noreply.github.com>
2023-08-26 15:44:50 +0100

    feat: added data segmenter fucntion based on threshold

---
+++ phddataimputation/dataSegmenterByThresholdExceedence.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 19:25:31 2023
+
+@author: goharshoukat
+
+Function to segment data if the gaps exceed a user defined value
+return a dict with a list of segmented data bins
+"""
+import pandas as pd
+from typing import Dict
+
+
+def dataSegmenterByThresholdExceedence(
+    df: pd.DataFrame, threshold: str
+) -> Dict[str, pd.DataFrame]:
+    df = df.dropna()  # preserve index
+    df["Date"] = pd.to_datetime(df["Date"])
+    df["time_diff"] = df["Date"].diff()
+    threshold = pd.Timedelta(threshold)
+    gaps_exceeding_threshold = df[df["time_diff"] > threshold]
+
+    return {"original": df, "segmented": gaps_exceeding_threshold}
+
+
+# df = pd.concat([
+#         df,
+#         (
+#             df.WindSpeed.isnull().astype(int)
+#             .groupby(df.WindSpeed.notnull().astype(int).cumsum())
+#             .cumsum().to_frame('consec_count')
+#         )
+#     ],
+#     axis=1
+# )
