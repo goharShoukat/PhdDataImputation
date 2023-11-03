c01d889b077ce3f1f8d4a7fcbbe5a7ab7a594eb7
Gohar Shoukat <25367760+goharShoukat@users.noreply.github.com>
2023-11-03 18:41:48 +0000

    feat: removed time distributed layers from conv1d due to incompatibility

--- phddataimputation/Model.py
+++ phddataimputation/Model.py
@@ -6,24 +6,20 @@ class ConvAndLSTMNet(tf.keras.Model):
     def __init__(self):
         super(ConvAndLSTMNet, self).__init__()
 
-        self.twentyFourHourPrior = layers.TimeDistributed(
-            layers.Conv1D(
-                filters=16,
-                kernel_size=2,
-                activation="relu",
-                strides=1,
-                input_shape=(24, 1),
-            )
+        self.twentyFourHourPrior = layers.Conv1D(
+            filters=16,
+            kernel_size=2,
+            activation="relu",
+            strides=1,
+            input_shape=(24, 1),
         )
 
-        self.twentyFourHourAfter = layers.TimeDistributed(
-            layers.Conv1D(
-                filters=16,
-                kernel_size=2,
-                activation="relu",
-                strides=1,
-                input_shape=(24, 1),
-            )
+        self.twentyFourHourAfter = layers.Conv1D(
+            filters=16,
+            kernel_size=2,
+            activation="relu",
+            strides=1,
+            input_shape=(24, 1),
         )
 
         self.concatenated = layers.Concatenate()
@@ -43,15 +39,16 @@ class ConvAndLSTMNet(tf.keras.Model):
         x1 = layers.MaxPooling1D(pool_size=3, strides=1, padding="valid")(x1)
         x1 = layers.Flatten()(x1)
 
-        x2 = self.twentyFourHourAfter(input_data2)
-        x2 = layers.MaxPooling1D(pool_size=3, strides=1, padding="valid")(x2)
-        x2 = layers.Flatten()(x2)
+        # x2 = self.twentyFourHourAfter(input_data2)
+        # x2 = layers.MaxPooling1D(pool_size=3, strides=1, padding="valid")(x2)
+        # x2 = layers.Flatten()(x2)
 
-        x = self.concatenated([x1, x2])
+        # x = self.concatenated([x1, x2])
+        x = self.concatenated([x1])
         x = self.fc1(x)
         x = self.dropout(x)
-        x = self.fc2(x)
-        x = self.lstm(x)
+        # x = self.fc2(x)
+        # x = self.lstm(x)
         output = self.output_layer(x)
 
         self.model = tf.keras.Model(inputs=[input_data1, input_data2], outputs=output)
