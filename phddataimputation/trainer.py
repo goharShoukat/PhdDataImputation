b06ea0932563a23068417b9941f5969433bce6eb
Gohar Shoukat <25367760+goharShoukat@users.noreply.github.com>
2023-11-03 18:07:57 +0000

    feat: normalised input data and reshaped before passing to trainer

--- phddataimputation/trainer.py
+++ phddataimputation/trainer.py
@@ -2,7 +2,13 @@ from Model import ConvAndLSTMNet
 import os
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
-x1 = pd.read_csv('data/trainingData/x1.csv')
-x2 = pd.read_csv('data/trainingData/x2.csv')
-y = pd.ready_csv('data/trainingData/y.csv')
+x1 = pd.read_csv("data/trainingData/x1.csv", header=None)
+x2 = pd.read_csv("data/trainingData/x2.csv", header=None)
+y = pd.read_csv("data/trainingData/y.csv", header=None)
+
+scalarX = MinMaxScaler(feature_range=(0, 1))
+X1 = scalarX.fit_transform(x1).T.reshape(-1, 24, 1)
+X2 = scalarX.fit_transform(x2).T.reshape(-1, 24, 1)
+Y = scalarX.fit_transform(y)
