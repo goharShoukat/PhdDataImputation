f93c500462102af80daa9fca19385f0f452cd5fe
Gohar Shoukat <25367760+goharShoukat@users.noreply.github.com>
2023-12-20 10:58:06 +0500

    fix: adjusted input_shape to LSTM consistent with documentation

--- phddataimputation/trainer_LSTMNet.py
+++ phddataimputation/trainer_LSTMNet.py
@@ -3,19 +3,26 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from utils import generateDirectory, featureGeneration
 
-features = 2
-neurons = 512
+features = 4
+neurons = 128
+scaling: bool = False
+
+
 x1, y = featureGeneration(
     pd.read_csv("data/trainingData/M2_1hour_Gaps_10%_Missing.csv")
     .dropna()
     .to_numpy(),
-     features,
-     False
+    features,
+    False,
 )
 
-scalarX = MinMaxScaler(feature_range=(0, 1))
-X1 = scalarX.fit_transform(x1).T.reshape(-1, features, 1)
-Y = scalarX.fit_transform(y.reshape(-1,1))
+if scaling:
+    scalar = MinMaxScaler(feature_range=(0, 1))
+    X1 = scalar.fit_transform(x1).T.reshape(-1, features, 1)
+    Y = scalar.fit_transform(y.reshape(-1, 1))
+else:
+    X1 = x1.T.reshape(-1, features, 1)
+    Y = y.reshape(-1, 1)
 
 model = LSTMNet(neurons)
 
@@ -25,8 +32,12 @@ metrics = ["mean_absolute_error"]
 
 input_shape = (features, 1)
 model.build(input_shape)
-pathToSaveModel = "models/{}/Model1-{}Neurons".format(features, neurons)
+
+pathToSaveModel = "models/{}/Model1-{}Neurons{}".format(
+    features, neurons, {True: "Scaled", False: "WithoutScale"}[scaling]
+)
 generateDirectory(pathToSaveModel)
+
 model.summary(path=pathToSaveModel)
 
 model.compile(
