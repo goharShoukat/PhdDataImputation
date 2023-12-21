c6078d4d8713d15de92821fe117bbbfc4161f07a
Gohar Shoukat <25367760+goharShoukat@users.noreply.github.com>
2023-12-21 21:55:47 +0500

    feat: added logger

--- phddataimputation/trainer_LSTMNet.py
+++ phddataimputation/trainer_LSTMNet.py
@@ -3,8 +3,12 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from utils import generateDirectory, featureGeneration
 from config import config1, config2, config3, config4
+import logging
 
-for con in config4():
+
+logging.basicConfig(filename="config1.log", level=logging.INFO, filemode="w")
+
+for con in config1():
     x1, y = featureGeneration(
         pd.read_csv("data/trainingData/M2_1hour_Gaps_10%_Missing.csv")
         .dropna()
@@ -48,3 +52,17 @@ for con in config4():
     model.train(X1, Y, epochs=20, batch_size=8)
 
     model.save_model(pathToSaveModel, format="tf")
+
+    logging.info(
+        "Features: {}, Neurons: {}, Scaling: {}".format(
+            con["features"], con["neurons"], con["scaling"]
+        )
+    )
+    logging.info("Loss: {}".format(model.getLoss()))
+    logging.info("Mean Absolute Error: {}".format(model.getMeanAbsoluteError()))
+    logging.info("Validation Loss: {}".format(model.getValLoss()))
+    logging.info(
+        "Validation Mean Absolute Error: {}\n\n\n\n".format(
+            model.getValMeanAbsoluteError()
+        )
+    )
