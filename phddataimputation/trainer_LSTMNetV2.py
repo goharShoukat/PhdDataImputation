f1f7b1cc0acdde88dfb16007ffe71568617fea9e
Gohar Shoukat <25367760+goharShoukat@users.noreply.github.com>
2024-02-18 18:07:06 +0000

    chore: restructure the LSTMNet with deep LSTMs

---
+++ phddataimputation/trainer_LSTMNetV2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+from datetime import date
+
+# https://medium.com/@vineet.pandya/use-tensorflow-lstm-for-time-series-forecasting-770ec789d2ce
+# https://github.com/Vineet214/TimeSeriesForecasting-LSTM/blob/main/Univariate%20Time%20Series%20Forecasting%20-%20LSTM.ipynb
